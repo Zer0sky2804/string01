15093bc30256f95d644d5d03681756770aaea970
oskeram.04 <oskeram.04@A222-14>
2022-03-24 11:00:35 +0100

    1,2

---
+++ string01/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace string01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string samohlasky = "aeiouyáéěíóúůý";
+            string souhlasky = "bcčdďfghjklmnpqrřsštťvwxzž";
+            string veta = textBox1.Text;
+            veta = veta.ToLower();
+            int psouhlasky = 0;
+            int psamohlasky = 0;
+            foreach(char a in veta)
+            {
+                if (samohlasky.Contains(a))
+                {
+                    psamohlasky++;
+                }
+                else
+                {
+                    if (souhlasky.Contains(a))
+                    {
+                        psouhlasky++;
+                    }
+                   
+                }
+            }
+            int ostatni = veta.Length - psamohlasky - psouhlasky;
+            label1.Text = "Ve vete je " + psamohlasky + " samohlasek, " + psouhlasky + " souhlasek a " + ostatni + " ostatnich znaku.";
+        }
+    }
+}
