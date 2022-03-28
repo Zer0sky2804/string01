be40e4a81708fc85d87b47a42133a0ffe24291f9
oskeram.04 <oskeram.04@A222-14>
2022-03-28 09:21:19 +0200

    2

--- string02/Form1.cs
+++ string02/Form1.cs
@@ -16,5 +16,28 @@ namespace string02
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int posun = int.Parse(textBox2.Text);
+            string sifra = textBox1.Text;
+            string vysledek="";
+            for(int i=0;i<sifra.Length;i++)
+            {
+                
+                char a = sifra[i];
+                if (a >= 'a' && a <= 'z' || a >= 'A' && a <= 'Z')
+                {
+                    if (a < 'a' && (int)a + posun > 'Z')
+                    {
+
+                    }
+                    int p = (int)a + posun;
+                    vysledek += (char)p;
+                    textBox1.Text = vysledek;
+                }
+                
+            }
+        }
     }
 }
